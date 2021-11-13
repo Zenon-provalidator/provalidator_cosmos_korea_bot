97ef4b6bec6ae2eb9053396fbb7c0d4c55b975ad
zenon-provalidator <zenon@provalidator.com>
2021-11-13 16:06:25 +0900

    new proposal update

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const CronJob = require('cron').CronJob
 
 //console.log(func.getProvalidatorDetail())
 //console.log(func.getMessage('cosmos'))
-//console.log(func.getProposal(57))
+console.log(func.getProposal(57))
 
 
 
@@ -13,23 +13,23 @@ const CronJob = require('cron').CronJob
 
 
 //loop
-const botJob = new CronJob(`*/5 * * * * *`, async function () {
-	let latestProposal = func.getLatestProposalNum() //마지막 프로포절 번호 가져오기
-
-	if(latestProposal !== 0 ){
-		let callProposalNum = latestProposal+1
-		let getProposal = func.getProposalFromServer(callProposalNum)
-		
-		if(typeof getProposal === "object"){
-			console.log()
-			return `Proposal #${callProposalNum} ${getProposal.title}\n\n ${getProposal.desc}\n\nhttps://www.mintscan.io/osmosis/proposals/${callProposalNum}`
-		} else if(getProposal === 203){
-			console.log(`${callProposalNum} proposal is not found`)
-		} else{
-			console.log(`server error`)
-		}
-	}else{
-		console.log(`latestProposal is 0`)
-	}
-	
-}).start()
+//const botJob = new CronJob(`*/5 * * * * *`, async function () {
+//	let latestProposal = func.getLatestProposalNum() //마지막 프로포절 번호 가져오기
+//
+//	if(latestProposal !== 0 ){
+//		let callProposalNum = latestProposal+1
+//		let getProposal = func.getProposalFromServer(callProposalNum)
+//		
+//		if(typeof getProposal === "object"){
+//			console.log()
+//			return `Proposal #${callProposalNum} ${getProposal.title}\n\n ${getProposal.desc}\n\nhttps://www.mintscan.io/osmosis/proposals/${callProposalNum}`
+//		} else if(getProposal === 203){
+//			console.log(`${callProposalNum} proposal is not found`)
+//		} else{
+//			console.log(`server error`)
+//		}
+//	}else{
+//		console.log(`latestProposal is 0`)
+//	}
+//	
+//}).start()
